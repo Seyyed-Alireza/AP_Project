4448ed298444f9f2225401f265ea1d644533eecd
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2025-07-06 14:33:04 +0330

    initialize product cards in product box

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,4 +1,18 @@
+# from django.shortcuts import render
+
+# def mainpage(request):
+#     return render(request, 'mainpage/mainpage.html')
+
+
+
+# for test
 from django.shortcuts import render
 
 def mainpage(request):
-    return render(request, 'mainpage/mainpage.html')
+    products = [
+        {'name': 'کرم مرطوب‌کننده', 'price': '120,000 تومان', 'image': 'images/product1.jpg'},
+        {'name': 'ضدآفتاب قوی', 'price': '180,000 تومان', 'image': 'images/product2.jpg'},
+        {'name': 'کرم شب', 'price': '150,000 تومان', 'image': 'images/product3.jpg'},
+    ]
+    return render(request, 'mainpage/mainpage.html', {'products': products})
+
