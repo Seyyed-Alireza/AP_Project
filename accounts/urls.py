33b9eea736379470fc4f587dad3f6b08997ea4f5
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2025-07-07 21:58:47 +0330

    update: some bugs in login page fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('register/', views.register, name='register'),
+    path('', RedirectView.as_view(url='/', permanent=False)),
     path('activate/<uidb64>/<token>/', views.activate_account, name='activate'),
     path('', views.home, name='mainpage')
 ]
--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,7 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    # path('accounts/login/', views.user_login, name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
+
+
+# path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
