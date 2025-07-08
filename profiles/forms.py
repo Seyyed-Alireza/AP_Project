56bc38e3ba74afe76a6fe76f96fc6a0141195654
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2025-07-09 00:29:13 +0330

    uploading profile picture completed

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,6 +5,21 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['phone_number', 'address', 'profile_picture']
+        labels = {
+            'phone_number': 'شماره موبایل',
+            'address': 'آدرس',
+            'profile_picture': 'تصویر پروفایل'
+        }
         widgets = {
             'address': forms.Textarea(attrs={'rows':3}),
         }
+
+    # def save(self, commit=True):
+    #     instance = super().save(commit=False)
+
+    #     if not instance.profile_picture:
+    #         instance.profile_picture = 'profile_pictures/default/default.svg'
+
+    #     if commit:
+    #         instance.save()
+    #     return instance
--- profiles/signals.py
+++ profiles/signals.py
@@ -7,3 +7,30 @@ from .models import UserProfile
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
+import os
+from django.db.models.signals import pre_save, post_delete
+
+@receiver(pre_save, sender=UserProfile)
+def delete_old_profile_picture(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old_instance = UserProfile.objects.get(pk=instance.pk)
+    except UserProfile.DoesNotExist:
+        return
+
+    old_file = old_instance.profile_picture
+    new_file = instance.profile_picture
+
+    if old_file and old_file != new_file:
+        if os.path.isfile(old_file.path) and 'default' not in old_file.name:
+            os.remove(old_file.path)
+
+@receiver(post_delete, sender=UserProfile)
+def delete_profile_picture_on_delete(sender, instance, **kwargs):
+    if instance.profile_picture and os.path.isfile(instance.profile_picture.path):
+        if 'default' not in instance.profile_picture.name:
+            os.remove(instance.profile_picture.path)
