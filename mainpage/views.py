c6b33c2d8050c69009ec25d9e36194b9324e6e75
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2025-07-15 17:14:00 +0330

    update for search function. better response for misspellings

--- mainpage/views.py
+++ mainpage/views.py
@@ -21,10 +21,64 @@ def mainpage(request):
 #####################################################################
 
 from difflib import SequenceMatcher
-def similarity(a, b):
+import re
+
+close_letters = {
+    'ض': ['1', 'ص', 'ش'],
+    'ص': ['2', 'ض', 'ث', 'س'],
+    'ث': ['3', 'ص', 'ق', 'ی'],
+    'ق': ['4', 'ث', 'ف', 'ب'],
+    'ف': ['5', 'ق', 'غ', 'ل'],
+    'غ': ['5', '6', 'ف', 'ع', 'ا'],
+    'ع': ['6', '7', 'غ', 'ه', 'ت'],
+    'ه': ['7', 'ع', 'خ', 'ن'],
+    'خ': ['8', 'ه', 'ح', 'م'],
+    'ح': ['9', 'خ', 'ج', 'ک'],
+    'ج': ['0', 'ح', 'چ', 'گ'],
+    'چ': ['ج', 'پ', 'گ', 'ک', 'و'],
+    'پ': ['چ', 'ن', 'و', 'د'],
+    'ش': ['ض', 'س', 'ظ'],
+    'س': ['ص', 'ش', 'ی', 'ط'],
+    'ی': ['ث', 'س', 'ب', 'ب'],
+    'ب': ['ق', 'ی', 'ل', 'ز'],
+    'ل': ['ف', 'ب', 'ا', 'ر'],
+    'ا': ['غ', 'ل', 'ت', 'ذ', 'آ'],
+    'آ': ['غ', 'ل', 'ت', 'ذ', 'ا'],
+    'ت': ['ع', 'ا', 'ن', 'د'],
+    'ن': ['ه', 'ت', 'م', 'و', 'پ'],
+    'م': ['خ', 'ن', 'ک', 'و'],
+    'ک': ['ح', 'م', 'گ', 'چ'],
+    'گ': ['ج', 'ک', 'چ'],
+    'ظ': ['ش', 'ط'],
+    'ط': ['س', 'ظ', 'ز', 'ژ'],
+    'ژ': ['ی', 'ط', 'ز', 'ر'],
+    'ز': ['ب', 'ژ', 'ر', 'ط'],
+    'ر': ['ل', 'ز', 'ذ'],
+    'ذ': ['ا', 'ر', 'د'],
+    'د': ['ت', 'ذ', 'پ'],
+    'و': ['م', 'چ', 'پ', 'ن'],
+}
+
+def similarity(user, db):
+    t_length = len(db)
+    each_letter = 100 / t_length
+    rate = 0
+    for i in range(min(len(user), len(db))):
+        if user[i] == db[i]:
+            rate += each_letter
+        elif db[i] in close_letters:
+            if user[i] in close_letters[db[i]]:
+                rate += 0.8 * each_letter
+    if len(user) != t_length and rate > 0.86:
+        rate /= (abs(len(user) - t_length) + 0.1)
+    return rate / 100
+
+def similar(a, b):
     return SequenceMatcher(None, a.lower(), b.lower()).ratio()
 def search(request):
-    full_query = request.GET.get('q')
+    full_query = request.GET.get('q').replace('\u200c', ' ')
+    full_query = re.sub(r's+', ' ', full_query).strip()
+    full_query = re.sub(r'[^a-zA-Zآ-ی0-9۰-۹\s]', '', full_query)
     query_words = full_query.lower().split()
     products = Product.objects.all()
     results = []
@@ -45,20 +99,23 @@ def search(request):
     print(query_words)
     for product in products:
         score = 0
-        product_name = product.name.lower()
-        product_brand = product.brand.lower()
+        product_name = product.name.lower().replace('\u200c', ' ')
+        product_brand = product.brand.lower().replace('\u200c', ' ')
 
         for word in query_words:
-            if word in product_name.split():
+            if word in product_name.split() or word in product_name.replace(' ', '') or product_name in word.replace(' ', ''):
                 score += 5
+                print('pdkwpe')
             elif word in product_brand:
                 score += 15
             else:
                 for p_word in product_name.split():
                     score += similarity(p_word, product_name) * 2
+                brand_similarity = similar(word, product_brand)
+                print(brand_similarity)
                 brand_similarity = similarity(word, product_brand)
-                if brand_similarity >= 0.75:
-                    score += brand_similarity * 10
+                print(brand_similarity)
+                score += 20 * brand_similarity ** 2
                 # score += similarity(word, product_brand) * (3.5 if brand_focus else 1.5)
 
         if score > 1:
@@ -67,6 +124,9 @@ def search(request):
     final_products = [r[0] for r in results][:500]
 
     print(len(final_products))
+
+    print(similar('نیوآ', 'نیوا'))
+    print(similarity('سی گل', 'سی گل'))
     return render(request, 'mainpage/mainpage.html', {'products': final_products})
 
 #####################################################################
@@ -106,37 +166,48 @@ def search(request):
 from django.http import JsonResponse
 
 def live_search(request):
-    full_query = request.GET.get('q')
+    full_query = request.GET.get('q').replace('\u200c', ' ')
     if not full_query:
         return JsonResponse({'results': []})
-
+    full_query = re.sub(r's+', ' ', full_query).strip()
+    full_query = re.sub(r'[^a-zA-Zآ-ی0-9۰-۹\s]', '', full_query)
     query_words = full_query.lower().split()
     products = Product.objects.all()
     results = []
+    brands = [
+        'سینره', 'نوتروژینا', 'لورآل', 'نیوآ', 'گارنیه',
+        'لاروش پوزای', 'سی‌گل', 'داو', 'فلورمار', 'ثمین',
+        'اون', 'بلومه', 'استی لادر', 'ادیپیرن', 'دکتر راشل',
+        'مَیبلین', 'بیودرما', 'اوردینری', 'کامان', 'الارو'
+    ]
 
     brand_focus = False
-    if 'برند' in query_words:
-        brand_focus = True
-        query_words.remove('برند')
-
+    # for i, word in enumerate(query_words):
+    #     if word in brands:
+    #         brand_focus = True
+    #         query_words.pop(i)
+    #     for brand in brands:
+    #         print(similarity(word, brand))
     for product in products:
         score = 0
-        product_name = product.name.lower()
-        product_brand = product.brand.lower()
+        product_name = product.name.lower().replace('\u200c', ' ')
+        product_brand = product.brand.lower().replace('\u200c', ' ')
 
         for word in query_words:
-            if word in product_name.split():
+            if word in product_name.split() or word in product_name.replace(' ', '') or product_name in word.replace(' ', ''):
                 score += 5
             elif word in product_brand:
-                score += 10 if brand_focus else 3
+                score += 15
             else:
                 for p_word in product_name.split():
                     score += similarity(p_word, product_name) * 2
-                score += similarity(word, product_brand) * (3.5 if brand_focus else 1.5)
+                brand_similarity = similar(word, product_brand)
+                brand_similarity = similarity(word, product_brand)
+                score += 20 * brand_similarity ** 2
+                # score += similarity(word, product_brand) * (3.5 if brand_focus else 1.5)
 
-        if score > 3:
+        if score > 1:
             results.append((product, score))
-
     results.sort(key=lambda x: x[1], reverse=True)
     seen_names = set()
     final_products = []
