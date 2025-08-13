6ee2b366dcd3e9ea7ba0eee6b4001e687b3040e3
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2025-08-13 16:24:10 +0330

    filter view bug fixed

--- mainpage/static/mainpage/both.js
+++ mainpage/static/mainpage/both.js
@@ -49,15 +49,13 @@ const filterButton = document.getElementById("filter-toggle")
 filterButton.addEventListener("click", function(){
     const filterMenu = document.getElementById("filter-menu")
     const aside = document.getElementById("aside")
-    if (window.innerWidth < 576) {
-        if (filterMenu.style.display !== "flex") {
-            filterMenu.style.display = "flex";
-            aside.style.border = "1px solid #ccc";
-            aside.style.padding = "10px 5px 10px"
-        } else {
-            filterMenu.style.display = "none";
-            aside.style.border = "none";
-            aside.style.padding = "10px 0 0"
-        }
+    if (filterMenu.style.display !== "flex") {
+        filterMenu.style.display = "flex";
+        aside.style.border = "1px solid #ccc";
+        aside.style.padding = "10px 5px 10px"
+    } else if (window.innerWidth < 576) {
+        filterMenu.style.display = "none";
+        aside.style.border = "none";
+        aside.style.padding = "10px 0 0"
     }
 });
